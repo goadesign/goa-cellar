352bd058ae72de0559a831eb06762a144b85b097
Brian Ketelsen <bketelsen@gmail.com>
2016-01-29 16:57:30 -0500

    fix import

--- design/api_definition.go
+++ design/api_definition.go
@@ -2,7 +2,7 @@ package design
 
 import (
 	. "github.com/goadesign/goa/design"
-	. "github.com/goadesign/goa/design/goadsl"
+	. "github.com/goadesign/goa/design/apidsl"
 )
 
 // This is the cellar application API design used by goa to generate
