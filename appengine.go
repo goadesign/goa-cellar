6dc34c68ede31f16d9f7726849e67a8c2a7a8538
Raphael Simon <simon.raphael@gmail.com>
2016-10-05 10:09:10 -0700

    Update goa-cellar for appengine

--- appengine.go
+++ appengine.go
@@ -10,6 +10,7 @@ import (
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa-cellar/app"
 	"github.com/goadesign/goa-cellar/controllers"
+	"github.com/goadesign/goa-cellar/store"
 	"github.com/goadesign/goa/logging/kit"
 	"github.com/goadesign/goa/middleware"
 )
@@ -30,16 +31,32 @@ func init() {
 
 	// Setup middleware
 	service.Use(middleware.RequestID())
+	service.Use(middleware.LogRequest(true))
 	service.Use(middleware.ErrorHandler(service, true))
 	service.Use(middleware.Recover())
 
+	// Setup database connection
+	db := store.NewDB()
+
 	// Mount account controller onto application
-	ac := controllers.NewAccount(service)
+	ac := controllers.NewAccount(service, db)
 	app.MountAccountController(service, ac)
 
 	// Mount bottle controller onto application
-	bc := controllers.NewBottle(service)
+	bc := controllers.NewBottle(service, db)
 	app.MountBottleController(service, bc)
+
+	// Mount public controller onto service
+	pc := controllers.NewPublic(service)
+	app.MountPublicController(service, pc)
+
+	// Mount js controller onto service
+	jc := controllers.NewJs(service)
+	app.MountJsController(service, jc)
+
+	// Mount swagger controller onto service
+	sc := controllers.NewSwagger(service)
+	app.MountSwaggerController(service, sc)
 
 	// Setup HTTP handler
 	http.HandleFunc("/", service.Mux.ServeHTTP)
